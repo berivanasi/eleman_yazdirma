4fa2271b788ce85202c66b77042467e9c23a0556
Berivan <83500587+berivanasi@users.noreply.github.com>
2022-04-13 12:15:11 +0300

    eleman_yazdırma

---
+++ eleman_yazdima/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace eleman_yazdima
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("******************************");
+
+            string[] isim = { "Aybike", "Beste", "Berika", "Tuğre", "Cenk" };
+    
+            
+            {
+
+                Console.WriteLine("isim:" + isim[0]);
+                Console.WriteLine("isim:" + isim[1]);
+                Console.WriteLine("isim:" + isim[2]);
+                Console.WriteLine("isim:" + isim[3]);
+                Console.WriteLine("isim:" + isim[4]);
+
+                Console.WriteLine("=======================");
+              
+                foreach (string ad in isim)
+             
+                {
+                    Console.WriteLine(ad);
+                }
+               
+
+
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
